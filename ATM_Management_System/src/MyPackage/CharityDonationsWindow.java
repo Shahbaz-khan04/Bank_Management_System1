86b14b28578bbca8458491fb122f1c278c4407b5
Shahbaz-khan04 <115358820+Shahbaz-khan04@users.noreply.github.com>
2023-05-26 14:39:42 +0500

    Add files via upload

---
+++ ATM_Management_System/src/MyPackage/CharityDonationsWindow.java
@@ -0,0 +1,105 @@
+package MyPackage;
+
+import javax.swing.*;
+
+import java.awt.BorderLayout;
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class CharityDonationsWindow extends JFrame {
+    int amount;
+    
+
+    User user;
+    public CharityDonationsWindow(User user) {
+    	this.user = user;
+    	
+    	// Setting up two panel
+    	JPanel panel = new JPanel();
+    	JPanel panel1 = new JPanel();
+    	JPanel panel2 = new JPanel();
+    	panel.setLayout(new BoxLayout(panel, FlowLayout.LEFT));
+    	panel2.setLayout(new BoxLayout(panel2, BoxLayout.Y_AXIS));
+    	
+        // Set up the frame
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setTitle("Charity Donations");
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        setSize(screenSize);
+        setLayout(new FlowLayout());
+
+        // Create components
+        JLabel donationLabel = new JLabel("Donation Amount:");
+        JLabel donationName = new JLabel("Organiztion Name:");
+        JTextField donationAmountField = new JTextField(15);
+        JTextField donationNameFeild = new JTextField(15);
+        JLabel space = new JLabel("     ");
+        JButton donateButton = new JButton("Donate");
+
+        // Add components in panel
+        panel.add(donationLabel);
+        panel.add(Box.createHorizontalStrut(20));
+        panel.add(donationAmountField);
+        
+        panel.add(donationName);
+        panel.add(Box.createHorizontalStrut(20));
+        panel.add(donationNameFeild);
+        
+        // Add panel and donate button in panel2
+        panel2.add(panel);
+        panel2.add(panel1);
+        panel2.add(space);
+        panel2.add(donateButton);
+        
+        // Setting left, top, bottom, right spacing of panel
+    	panel2.setBorder(BorderFactory.createEmptyBorder(200, 400, 450, 400));
+    	
+    	// Setting layout of frame and adding panel
+    	setLayout(new BorderLayout());
+    	add(panel2, BorderLayout.CENTER);
+
+        // donateButton button action listener
+        donateButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+            	
+            	// converting value coming from donationAmountField into int
+            	amount = Integer.parseInt(new String(donationAmountField.getText()));
+            	String orgName = new String(donationNameFeild.getText());
+            	
+            	// Set up the frame
+                JFrame frame = new JFrame("Charity Donation");
+                frame.setSize(600, 150);
+                frame.setLocationRelativeTo(null);
+                
+                // checking for valid donation ammount
+            	if (amount > 0 && user.getBalance() >= amount) {
+                    user.setBalance(user.getBalance() - amount);
+                    JLabel lbl1 = new JLabel("Charity has been given to "+orgName +", "+amount+" has been deducted. Updated Balance: " + user.getBalance());
+                    lbl1.setHorizontalAlignment(SwingConstants.CENTER);
+                    frame.getContentPane().setLayout(new BorderLayout());
+                    frame.getContentPane().add(lbl1, BorderLayout.CENTER);
+                    frame.setVisible(true);
+                } else {
+                	JLabel lbl1;
+                	if(amount >= 0) {
+                		lbl1 = new JLabel("Insufficiect Funds");
+                	} else if(amount == 0) {
+                		lbl1 = new JLabel("Ammount cannot be zero");
+                	}
+                	else {
+                		lbl1 = new JLabel("Please enter correct ammount");
+                	}
+                    lbl1.setHorizontalAlignment(SwingConstants.CENTER);
+                    frame.getContentPane().setLayout(new BorderLayout());
+                    frame.getContentPane().add(lbl1, BorderLayout.CENTER);
+                    frame.setVisible(true);
+                }
+            }
+        });
+    }
+}
